dbd68b9d2489f212f4676bcc5051df25938bdcaa
Adam.Klis <adam.klis@asseco.pl>
2020-12-31 11:07:29 +0100

    azure prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  APIEndpoint: 'https://classroombookingsystembe.azurewebsites.net/api'
+    APIEndpoint: 'https://classroombookingsystembe.azurewebsites.net/api'
 };
