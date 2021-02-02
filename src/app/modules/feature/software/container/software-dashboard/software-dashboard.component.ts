90866843312214a124c14e4d25b812c1eaa3a29c
Adam.Klis <adam.klis@asseco.pl>
2021-02-02 17:44:41 +0100

    software filters

--- src/app/modules/feature/software/container/software-dashboard/software-dashboard.component.ts
+++ src/app/modules/feature/software/container/software-dashboard/software-dashboard.component.ts
@@ -1,6 +1,11 @@
 import { SoftwareService } from '../../service/software.service';
 import { ISoftware } from '../../interface/software.interface';
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ITag } from 'src/app/modules/shared/component/tag-bar/tag.interface';
+import { SortOrder } from 'src/app/modules/shared/enum/sort-order.enum';
+import { Filter } from 'src/app/modules/shared/model/filter';
+import { Sort } from 'src/app/modules/shared/model/sort';
 
 @Component({
   selector: 'cbs-software-dashboard',
@@ -9,12 +14,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoftwareDashboardComponent implements OnInit {
 
-  public softwareList: ISoftware[];
+  public $tags: BehaviorSubject<ITag[]> = new BehaviorSubject<ITag[]>([]);
+  public $software: BehaviorSubject<ISoftware[]> = new BehaviorSubject<ISoftware[]>([]);
 
-  constructor(private softwareService: SoftwareService) { }
+  constructor(
+    private softwareService: SoftwareService
+  ) { }
 
   ngOnInit(): void {
-    this.softwareService.getSoftwareList().subscribe(softwareList => this.softwareList = softwareList);
+    this.tagsChanged();
+  }
+
+  public tagsChanged(tags?: ITag[]): void{
+    if (tags){
+      this.$tags.next(tags);
+    }
+    const sort = [new Sort('name', SortOrder.ASCEND)];
+
+    const filter = this.$tags.value.map(tag => {
+      if (tag.category === 'keyword_int'){
+        return new Filter('quantity', tag.value);
+      } else {
+        return new Filter('name', tag.value);
+      }
+    });
+
+    this.softwareService.getSoftwareList(filter, sort).toPromise().then(software => this.$software.next(software));
+  }
+
+  public searchChanged(searchText: string): void{
+
   }
 
 }
